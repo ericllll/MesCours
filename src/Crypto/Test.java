0969c436b1b3b0f8204e49f935cef47364cdc597
ericllll <ericllll@outlook.com>
2015-03-26 15:39:00 +0100

    Modifications de Vigenere Reste à recoder correctement et à rendre les méthodes réutilisables. A permis en l'état de décoder le vigenere cypher.

--- src/Crypto/Test.java
+++ src/Crypto/Test.java
@@ -6,13 +6,20 @@ public class Test {
 		vigenere essai = new vigenere();
 		essai.lireFichier();
 		/*for (int i=1; i<=13; i++){
-			essai.calculerDistribution(i, 1);
+			essai.calculerDistribution(i, 4);
 		}*/
-
 		
-		essai.calculerDistribution(7, 1);
+		//essai.calculerDistribution(7, 7);
+
 		//essai.trouverCle();
-		//essai.decoder();
+		
+		essai.decoder();
+		
+		/*for (int i=32; i<=63; i++){
+			System.out.print("ligne : " + i + " :   ");
+			essai.decoder2(i);
+			System.out.println("");
+		}*/
 		
 	}
 
--- src/Crypto/vigenere.java
+++ src/Crypto/vigenere.java
@@ -14,17 +14,23 @@ public class vigenere {
 	int longueurTexte = 0;
 	// longueur de la cle
 	int longueurCle = 0;
-	int [] distrib = new int[256];
-	ArrayList<Byte> tab = new ArrayList<Byte>();
+	//int [] distrib = new int[256];
+	ArrayList<Integer> tab = new ArrayList<Integer>();
 
 	public void lireFichier(){
 		FileInputStream fis =null;
 		byte [] c = new byte[2];
+		//String c = "00";
 		try {
 			fis = new FileInputStream(new File("src/Crypto/Data/entree.txt"));
 			while ((fis.read(c, 0, c.length))>=0){
-				System.out.print((char)c[0]*16+(char)c[1] + " ");
-				tab.add((byte)((byte)c[0]*16+(byte)c[1]));
+				char [] copie = new char[2];
+				copie[0] = (char)c[0];
+				copie[1] = (char)c[1];
+				//System.out.println(String.copyValueOf(copie));
+				tab.add((hex2decimal(String.copyValueOf(copie))));
+				System.out.print(hex2decimal(String.copyValueOf(copie)) + " ");
+				//tab.add((byte)((byte)c[0]*16+(byte)c[1]));
 				longueurTexte++;
 			}
 			System.out.println("");
@@ -40,10 +46,11 @@ public class vigenere {
 			System.out.println("Erreur de parametres");
 			return;
 		}
-		//int [] distrib = new int[256];
+		int [] distrib = new int[256];
 		int pos = offset-1;
 		int nbrElements = 0;
 		while (pos<tab.size()){
+			//System.out.println(tab.get(pos));
 			distrib[tab.get(pos)]++;
 			pos+=longueurCle;
 			nbrElements++;
@@ -55,49 +62,76 @@ public class vigenere {
 		//System.out.println(1.0/256.0);
 		System.out.println("longueur cle : " + longueurCle + ". Offset : " + offset + " -");
 		System.out.println("la somme vaut " + somme + " et l'ecart vaut " + Math.abs(somme-(1.0/256.0)));
+		trouverCle(distrib, longueurCle, offset);
 	}
-	public void trouverCle(){
+	
+	public void trouverCle(int [] distrib, int longueurCle, int offset){
 		int max =0;
-		for(int i=0; i<256; i++) {
+		for(int i=255; i>=0; i--) {
 			if(distrib[i]>distrib[max]){
 				max=i;
 			}
 		}
-		byte encode=(byte)max;
-		byte cle=0;
-		byte decode=101;
-		while((encode^cle)!=decode){
+		int cle=0;
+		int decode=101;
+		while((max^cle)!=decode){
 			cle++;
 		}
+		//cle=187; // essai d'une cle 
 		System.out.println("la cle est : "+cle);
+		int pos=offset-1;
+		int nb=0; // nombre de caractères hors intervalle  32 - 127
+		for (cle=0; cle<256; cle++){ // boucle optionnelle
+		pos = offset-1;
+		nb = 0;
+		while (pos<tab.size()){
+			int temp = cle^(tab.get(pos));
+			if (temp<32 || temp >127){
+				nb++;
+			}
+			pos+=longueurCle;
+		}
+		System.out.println("Le nombre de caractère hors intervalle est : " + nb + ". (Cle = " + cle);
+		} // fin boucle optionnelle
 	}
 	
 	public void decoder(){
-		byte [] cle = {33, 86, 82, 38, 35, 80 ,33};
+		int [] cle = {186, 31, 145, 178, 83, 205 , 62};
 		int pos=0;
 		int i=0;
 		while (pos<tab.size()){
-			byte temp = (byte)(cle[i]^(tab.get(pos)));
+			int temp = cle[i]^(tab.get(pos));
 			System.out.print((char)temp);
 			i++;
 			if (i==7){
 				i=0;
+				//System.out.println("");
 			}
 			pos++;
 		}
 	}
+	
+	public void decoder2(int a){
+		int pos=6;
+		while (pos<tab.size()){
+			int temp = a^(tab.get(pos));
+			System.out.print((char)temp);
+			pos+=7;
+		}
+	}
+
+	
 	public static int hex2decimal(String s) {
 		String digits = "0123456789ABCDEF";
-		s = s.toUpperCase();
+		//s = s.toUpperCase();
 		int val = 0;
-		for (int i = 0; i<s.length(); i++) {
+		for (int i = 0; i<2; i++) {
 			char c = s.charAt(i);
 			int d = digits.indexOf(c);
 			val = 16*val + d;
 		}
 		return val;
 	}
-	
 	
 }
 	
