b07081b3ca34ab2dc469b775484771acd7b4ebe3
ericllll <ericllll@outlook.com>
2015-03-29 00:47:09 +0100

    Classe NombreBinaire complétée mais non testée

--- src/Crypto/NombreBinaire.java
+++ src/Crypto/NombreBinaire.java
@@ -1,7 +1,7 @@
 package Crypto;
 
 public class NombreBinaire {
-	private int nbrBinaire = 0b10101100;
+	private int nbrBinaire;
 	
 	public NombreBinaire(int nbr){
 		nbrBinaire = nbr;
@@ -11,6 +11,64 @@ public class NombreBinaire {
 		nbrBinaire = (int)nbr;
 	}
 	
+	public NombreBinaire(String nbr){
+		String str1 = "0123456789ABCDEF";
+		String str2 = "01";
+		nbr.toUpperCase();
+		boolean test = false;
+		int i = 0;
+		while(i<nbr.length() && test==false){
+			if(str2.indexOf(nbr.charAt(i))==-1){
+				test=true;
+			}
+			i++;
+		}
+		if(test==false){
+			nbrBinaire = bin2Decimal(nbr);
+		} else {
+			i=0;
+			test=false;
+			while(i<nbr.length() && test==false){
+				if(str1.indexOf(nbr.charAt(i))==-1){
+					test=true;
+				}
+				i++;
+			}
+			if(test=false){
+				nbrBinaire = hex2Decimal(nbr);
+			} else {
+				System.out.println("La chaine de caractère contient des caractères non valides pour un format binaire ou hexa.");
+			}
+		}
+	}
+	
+	public NombreBinaire xor(NombreBinaire nbBin){
+		NombreBinaire res = new NombreBinaire(nbBin.getNbrInteger()^this.nbrBinaire);
+		return res;
+	}
+	
+	public static int hex2Decimal(String s){
+		String digits = "0123456789ABCDEF";
+		s = s.toUpperCase();
+		int val = 0;
+		for(int i=0; i<s.length(); i++){
+			char c = s.charAt(i);
+			int d = digits.indexOf(c);
+			val = 16*val + d;
+		}
+		return val;
+	}
+	
+	public static int bin2Decimal(String s){
+		String digits = "01";
+		int val = 0;
+		for (int i=0; i<s.length(); i++){
+			char c = s.charAt(i);
+			int d = digits.indexOf(c);
+			val = 2*val + d;
+		}
+		return val;
+	}
 	//
 	// Accesseurs du nombre binaire
 	//
@@ -19,12 +77,12 @@ public class NombreBinaire {
 	}
 	public String getNbrStringHex(){
 		String str = new String();
-		
+		str = Integer.toHexString(nbrBinaire);
 		return str;
 	}
 	public String getNbrStringBin(){
 		String str = new String();
-		
+		str = Integer.toBinaryString(nbrBinaire);
 		return str;
 	}
 	
